de2e2a21f5a73351bc8e432fca1f0f0371b18ff3
ashwoodall[user] <ash.woodall@gmail.com>
2018-04-03 20:01:01 -0500

    feat(Avatar): Add initials
    This PR adds the ability for a name prop to be passed to Avatar and
    creates initials if an image src is not definied. It handles passed
    first + last name, first (single)  name, or initials.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Circular } from './Circular';
 import { Image } from './Image';
 
+const createInitials = (name) => {
+  if (name.length <= 2) {
+    return name;
+  }
+
+  const nameArr = name.split(' ');
+  const initials = nameArr.length === 1 ? nameArr[0][0] : nameArr[0][0] + nameArr[1][0];
+  
+  return initials;
+};
+
 export const Avatar = Circular.extend.attrs({
-  children: props => (props.src ? <Image src={props.src} /> : props.children),
-}) `
-  // This is just here because we have to have some sort of styles applied at this level.
-  text-align: left;
-`;
+  children: props => (props.src ? <Image src={props.src} /> : createInitials(props.name)),
+});
